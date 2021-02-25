aa46d84a4ed0cd9dc0dadfca2639188278a6ab88
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-02-25 22:15:37 +1100

    first working version with hard encoded github names

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import React, { Component } from "react";
+import Users from "./components/users/Users";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar title="Github title" icon='fab fa-github'/>
+        <h1>Hello world</h1>
+        <div className="container">
+          <Users />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
+
+export class Navbar extends Component {
+    static defaultProps = {
+        icon:'fab fa-github',
+        title:'Github finder'
+    }
+
+    static propTypes = {
+        icon:PropTypes.string.isRequired,
+        title:PropTypes.string.isRequired
+
+    }
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon}/> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
